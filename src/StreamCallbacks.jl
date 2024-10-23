4215ef781273fa7032ea20cb8fb4b05c4ce3d230
J S <49557684+svilupp@users.noreply.github.com>
2024-10-23 20:54:41 +0100

    add precomp

--- src/StreamCallbacks.jl
+++ src/StreamCallbacks.jl
@@ -1,6 +1,7 @@
 module StreamCallbacks
 
 using HTTP, JSON3
+using PrecompileTools
 
 export StreamCallback, StreamChunk, OpenAIStream, AnthropicStream, streamed_request!
 include("interface.jl")
@@ -12,5 +13,9 @@ include("stream_openai.jl")
 include("stream_anthropic.jl")
 
 include("stream_ollama.jl")
+
+@compile_workload begin
+    include("precompilation.jl")
+end
 
 end
