19df7e1dd5974dd886bf5049f80953819749d21a
SixZero <havliktomi@hotmail.com>
2025-11-17 16:27:48 +0100

    The new response endpoint stream support with ResponseStream.
    With reasoning token stream as italic streamed.

--- src/StreamCallbacks.jl
+++ src/StreamCallbacks.jl
@@ -3,8 +3,9 @@ module StreamCallbacks
 using HTTP, JSON3
 using PrecompileTools
 
-export StreamCallback, StreamChunk, OpenAIStream, AnthropicStream, OllamaStream,
+export StreamCallback, StreamChunk, OpenAIStream, AnthropicStream, OllamaStream, ResponseStream,
        streamed_request!
+
 include("interface.jl")
 
 include("shared_methods.jl")
@@ -14,6 +15,8 @@ include("stream_openai.jl")
 include("stream_anthropic.jl")
 
 include("stream_ollama.jl")
+
+include("stream_response.jl")
 
 @compile_workload begin
     include("precompilation.jl")
